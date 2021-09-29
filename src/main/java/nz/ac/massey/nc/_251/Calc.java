f021a2713672f089f667b6597a8339b8be052ea5
Sun Hao <sh15620315298@163.com>
2021-09-29 15:56:17 +0800

    Task 1

---
+++ src/main/java/nz/ac/massey/nc/_251/Calc.java
@@ -0,0 +1,11 @@
+package nz.ac.massey.nc._251;
+
+public class Calc {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public  static int subtract(int a, int b){ return a - b;}
+
+}
---
+++ src/test/java/nz/ac/massey/nc/_251/TestCalc.java
@@ -0,0 +1,22 @@
+package nz.ac.massey.nc._251;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalc {
+    @Test
+    public void testAdd() throws InterruptedException {
+        int a = 1;
+        int b = 2;
+        int result_add = Calc.add(a, b);
+        Assertions.assertEquals(a + b, result_add);
+    }
+//    @Test
+//    public  void testSubtract() throws InterruptedException {
+//        int a = 1;
+//        int b = 2;
+//        int result_subtract = Calc.subtract(a, b);
+//        Assertions.assertEquals(a - b, result_subtract);
+//    }
+
+}
